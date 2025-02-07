556694f7cd88093b6aa328445e768196db90ed03
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-07 16:48:22 -0700

    eliminé algunos archivos

--- condicionales_ciclos.ps1
+++ condicionales_ciclos.ps1
@@ -1,3 +1,32 @@
+# Los condicionales funcionan exactamente que en cualquier lenguaje de programación actual
+if ($condition) {
+    Write-Output "Condition verdadera"
+} else {
+    Write-Output "Condition falsa"
+}
+
+$numero = 2
+if ($numero -ge 3) {
+    Write-Output "Numero mayor que tres"
+} elseif ($numero -lt 2) {
+    Write-Output "Numero menor que dos"
+} else {
+    Write-Output "Numero igual a dos"
+}
+
+# Operador ternario
+# $PSVersionTable para checar la version
+# Funciona solo en versiones >= 7.0, en mi caso tengo la 5.1
+# ($numero -ge 2) ? (Write-Output "Numero mayor a dos") : (Write-Output "Numero menor a dos")
+
+# if (Test-Path $path) {
+#     Write-Output "La ruta existe"
+# } else {
+#     Write-Output "La ruta no existe"
+# }
+
+# Con operador ternario quedaría algo así
+# (Test-Path $path) ? "La ruta existe" : "La ruta no existe"
 # Cualquier patrón que coincida con el valor establecido en el switch case
 # En este caso son puros enteros y se utiliza una 
 switch(3){
@@ -62,3 +91,39 @@ switch -Regex ($url, $email, $email2)
     '~ftp://.*$' { "[$_] es una direccion ftp" }
     '~(http[s]?)//.*$' { "[$_] es una direccion web, que utiliza [$($matches[1])]" }
 }
+# El valor de la derecha se convierte al tipo del de la izquierda
+# 1 === 1 True
+1 -eq "1.0"
+# "1.0" === "1" False
+"1.0" -eq 1
+# Comparador and
+1 -eq "1.0" -and 1 -gt 2 # Falso
+"1.0" -eq 1 -and 1 -gt -1 # True
+"1.0" -eq 2 -xor 2 -eq 3 # Falso
+-not (1 -eq "1.0" -and 1 -gt 2) # Falso NOT -> True
+# A diferencia de los lenguajes de programación comunes en los que se utilizan (&&, ||) u operadores directamente como >, <, >=, <=
+# En powershell se utilizan de manera un poco distinta
+# Por ejemplo, también hay algunas similitudes con sql, como el uso de like al hacer comparaciones con strings
+# Bucle for
+# Solo se aumenta el valor de i, j se mantiene en cero durante todo el ciclo
+# Funciona igual que el de cualquier lenguaje de programación convencional -> variable de control, condición para detenerlo; acción a repetir
+for (($i = 0), ($j = 0); $i -lt 5; $i++)
+{
+    "`$i:$i"
+    "`$j:$j"
+}
+# Mismo ciclo, se aumenta el valor de ambas variables
+for ($($i = 0; $j = 0); $i -lt 5; $($i++;$j++))
+{
+    "`$i:$i"
+    "`$j:$j"
+}
+
+# ForEach
+$numeros = 1..10
+# El bucle funciona sobre colecciones e itera a través de los elementos de este
+forEach($numero in $numeros){
+    echo ($numero * $numero);
+}
+
+# While
