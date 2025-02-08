7732ed8238e2b4fffdf0923d040f1c1d3c57ea63
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-08 12:38:18 -0700

    cambié la función por una regex, ya que la función no validaba correctamente todos los casos

--- practica-dns/wsconfig/wsconfig.ps1
+++ practica-dns/wsconfig/wsconfig.ps1
@@ -3,19 +3,15 @@ $dominio = Read-Host "Ingresa el dominio a registar"
 #$ip = (Get-NetIpAddress -InterfaceIndex 6 -AddressFamily IPv4).IPAddress
 $zonefile = $dominio + ".dns"
 
-function Es-IpValida{
+function Es-IpValida {
     Param([Parameter(Mandatory)][string]$ip)
-    forEach($byte in $ip.Split(".")){
-        if($byte -gt 255){
-            return $false
-        }
-    }
-    return $true
+    return $ip -match "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
 }
 
 function Es-DominioValido($dominio){
     return $dominio -match "^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$"
 }
+
 
 if((Es-IpValida -ip $ip) -and (Es-DominioValido -dominio $dominio)){
     try{
@@ -29,7 +25,6 @@ if((Es-IpValida -ip $ip) -and (Es-DominioValido -dominio $dominio)){
     catch{
         echo "Ha ocurrido un error, verifique los datos ingresados"
     }
-    echo "Datos correctos"
 }
 else{
     echo "Has ingresado una ip o dominio con el formato incorrecto"
