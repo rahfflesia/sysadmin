3fb88ec44eb979831b133c8c9ba3174dac45d45d
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-23 13:14:33 -0700

    añadí comando en caso de que el servicio esté enmascarado

--- modulosus/funcionesPrincipales.sh
+++ modulosus/funcionesPrincipales.sh
@@ -95,6 +95,8 @@ function mainDhcp(){
 function mainSsh(){
     sudo apt-get upgrade
     sudo apt install -y ssh
-    sudo systemctl status ssh
+    sudo systemctl unmask ssh
+    sudo systemctl enable ssh
     sudo systemctl start ssh.service
+    sudo systemctl status ssh
 }
