03b75ecbe02a1c1b427b97835bc5f8a9822cb6fb
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-28 14:32:56 -0700

    v2 script ftp

--- ftp/wsconfig/ftp.ps1
+++ ftp/wsconfig/ftp.ps1
@@ -17,6 +17,7 @@ function Get-ADSI(){
 
 function Crear-SitioFtp([string]$nombreSitio, [int]$puerto = 21, [string]$ruta){
     New-WebFtpSite -Name $nombreSitio -Port $puerto -PhysicalPath $ruta
+    return $nombreSitio
 }
 
 function Crear-Grupo([string]$nombreGrupo, [string]$descripcion){
@@ -74,7 +75,7 @@ function Habilitar-Autenticacion([string]$nombreSitio, [string]$nombreGrupo){
     Add-WebConfiguration @param
 }
 
-function Permitir-SSL([string]$rutaSitoFtp){
+function Permitir-SSL([string]$rutaSitioFtp){
     $SSLPolicy = @(
         "ftpServer.security.ssl.controlChannelPolicy",
         "ftpServer.security.ssl.dataChannelPolicy"
@@ -104,6 +105,10 @@ if(-not(Check-WindowsFeature "Web-FTP-Server") -and -not(Check-WindowsFeature "W
 
 Import-Module WebAdministration
 
+$nombreSitio = "Servidor FTP"
+$rutaFisicaFTP = "C:\Users\Administrador\Servidor-FTP\Publica"
+$rutaSitioIIS = "IIS:\Sites\$nombreSitio"
+Crear-SitioFtp -nombreSitio $nombreSitio -ruta $rutaFTP
 Crear-Grupo -nombreGrupo "reprobados" -descripcion "Grupo FTP de reprobados"
 Crear-Grupo -nombreGrupo "recursadores" -descripcion "Grupo FTP de recursadores"
 
@@ -131,7 +136,18 @@ while($true){
             1 {
                 $usuario = Read-Host "Ingresa el nombre de usuario"
                 $password = Read-Host "Ingresa la contrasena" -AsSecureString
-                $grupo = Read-Host "Ingresa el grupo al que pertenecera el usuario"
+                $grupo = Read-Host "Ingresa el grupo al que pertenecera el usuario (reprobados/recursadores)"
+                try{
+                    Crear-UsuarioFtp $usuario $password
+                    Agregar-UsuarioAGrupoFTP $usuario $grupo
+                    Habilitar-Autenticacion $nombreSitio $nombreGrupo
+                    Permitir-SSL $rutaSitioIIS
+                    Establecer-PermisosNtfs $rutaFisicaFTP $grupo $nombreSitio
+                    echo "Configuracion establecida correctamente"
+                }
+                catch{
+                    echo $Error
+                }
             }
             2 {
                 $usuarioACambiar = Read-Host "Ingresa el usuario a cambiar de grupo"
