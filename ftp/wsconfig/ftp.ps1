61d6bdd2abb8acb8a20c3a331ef738d376257369
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-02-28 14:02:59 -0700

    v1 script ftp powershell

--- ftp/wsconfig/ftp.ps1
+++ ftp/wsconfig/ftp.ps1
@@ -10,12 +10,102 @@ function Check-WindowsFeature {
     }
 }
 
+function Get-ADSI(){
+    $ADSI = [ADSI]"WinNT://$env:ComputerName"
+    return $ADSI
+}
+
+function Crear-SitioFtp([string]$nombreSitio, [int]$puerto = 21, [string]$ruta){
+    New-WebFtpSite -Name $nombreSitio -Port $puerto -PhysicalPath $ruta
+}
+
+function Crear-Grupo([string]$nombreGrupo, [string]$descripcion){
+    if(-not(Get-LocalGroup -Name $nombreGrupo)){
+        $ADSI = Get-ADSI
+        $grupoUsuarios = $ADSI.Create("Group", "$nombreGrupo")
+        $grupoUsuarios.SetInfo()
+        $grupoUsuarios.Description = $descripcion
+        $grupoUsuarios.SetInfo()
+        return $nombreGrupo
+    }
+    else{
+        Get-LocalGroup -ErrorAction SilentlyContinue
+    }
+    return $null
+}
+
+function Crear-UsuarioFtp([string]$usuario, [string]$contrasena){
+    $ADSI = Get-ADSI
+    $usuarioFtp = $ADSI.Create("User", "$usuario")
+    $usuarioFtp.SetInfo()
+    $usuarioFtp.SetPassword("$contrasena")
+    $usuarioFtp.SetInfo()
+}
+
+function Agregar-UsuarioAGrupoFTP([string]$usuario, [string]$nombreGrupo){
+    if($nombreGrupo -ne $null){
+        $cuentaUsuario = New-Object System.Security.Principal-NTAccount("$usuario")
+        $SID = $cuentaUsuario.Translate([System.Security.Principal.SecurityIdentifier])
+        $grupo = [ADSI]"WinNT://$env:ComputerName/$nombreGrupo,Group"
+        $user = [ADSI]"WinNT://$SID"
+        $grupo.Add($user.Path)
+    }
+    else{
+        echo "Error, grupo con valor nulo, puede que el grupo ya exista"
+    }
+}
+
+function Habilitar-Autenticacion([string]$nombreSitio, [string]$nombreGrupo){
+    $rutaSitioFtp = "IIS:\Sites\$nombreSitio"
+    $basicAuth = "ftpServer.security.authentication.basicAuthentication.enabled"
+    Set-ItemProperty -Path $rutaSitioFtp -Name $basicAuth -Value $True
+
+    $Param = @{
+        Filter = "/system.ftpServer/security/authorization"
+        Value = @{
+            accessType = "Allow"
+            roles = "$nombreGrupo"
+            permissions = 3
+        }
+    }
+
+    PSPath = "IIS:\"
+    Location = $nombreSitio
+    Add-WebConfiguration @param
+}
+
+function Permitir-SSL([string]$rutaSitoFtp){
+    $SSLPolicy = @(
+        "ftpServer.security.ssl.controlChannelPolicy",
+        "ftpServer.security.ssl.dataChannelPolicy"
+    )
+    Set-ItemProperty -Path $rutaSitioFtp -Name $SSLPolicy[0] -Value $false
+    Set-ItemProperty -Path $rutaSitioFtp -Name $SSLPolicy[1] -Value $false
+}
+
+function Establecer-PermisosNtfs([string]$rutaSitio, [string]$nombreGrupo, [string]$nombreSitio){
+    $cuentaUsuario = New-Object System.Security.Principal.NTAccount("$nombreGrupo")
+    $reglaDeAcceso = [System.Security.AccessControl.FileSystemAccessRule]::new($cuentaUsuario,
+        "ReadAndExecute",
+        "ContainerInherit,ObjectInherit",
+        "None",
+        "Allow"
+    )
+    $ACL = Get-Acl -Path $rutaSitio
+    $ACL.SetAccessRule($reglaDeAcceso)
+    $ACL | Set-Acl -Path $rutaSitio
+    Restart-WebItem "IIS:\Sites\$nombreSitio" -Verbose
+}
+
 if(-not(Check-WindowsFeature "Web-FTP-Server") -and -not(Check-WindowsFeature "Web-Server")){
     Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature
     Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools
 }
 
 Import-Module WebAdministration
+
+Crear-Grupo -nombreGrupo "reprobados" -descripcion "Grupo FTP de reprobados"
+Crear-Grupo -nombreGrupo "recursadores" -descripcion "Grupo FTP de recursadores"
 
 while($true){
     echo "Menu"
