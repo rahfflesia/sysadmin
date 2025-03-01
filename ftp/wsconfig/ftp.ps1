832c540e4a851474249a0bec6620a3ad1fd5ab33
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-03-01 14:12:04 -0700

    refactor

--- ftp/wsconfig/ftp.ps1
+++ ftp/wsconfig/ftp.ps1
@@ -1,136 +1,41 @@
-function Check-WindowsFeature {
-    [CmdletBinding()]
-    param(
-        [Parameter(Position=0,Mandatory=$true)] [string]$FeatureName 
-    )  
-  if((Get-WindowsOptionalFeature -FeatureName $FeatureName -Online).State -eq "Enabled") {
-        return $true
-    } else {
-        return $false
-    }
-}
+Install-WindowsFeature Web-Server -IncludeManagementTools
+Install-WindowsFeature Web-Ftp-Server -IncludeAllSubFeature
+Install-WindowsFeature Web-Basic-Auth
 
-if(-not(Check-WindowsFeature "Web-FTP-Server") -and -not(Check-WindowsFeature "Web-Server") -and -not(Check-WindowsFeature "Web-Basic-Auth")){
-    Install-WindowsFeature Web-FTP-Server -IncludeAllSubFeature
-    Install-WindowsFeature Web-Server -IncludeAllSubFeature -IncludeManagementTools
-    Install-WindowsFeature Web-Basic-Auth
-}
+mkdir "C:\FTP"
 
-function crearGrupo([string]$nombreGrupo){
-    if(-not(Get-LocalGroup -Name $nombreGrupo)){
-        New-LocalGroup -Name $nombreGrupo -Description "Grupo FTP de $nombreGrupo"
-    }
-}
+New-WebFtpSite -Name "FTP" -Port 21 -PhysicalPath "C:\FTP\" -Force
 
-Import-Module WebAdministration
+# Creación del grupo
+$FTPUserGroupName = "MiembrosFTP"
+$ADSI = [ADSI]"WinNT://$env:ComputerName"
+$FTPUserGroup = $ADSI.Create("Group", "$FTPUserGroupName")
+$FTPUserGroup.SetInfo()
+$FTPUserGroup.Description = "Usuarios FTP"
+$FTPUserGroup.SetInfo()
 
-function Crear-SitioFtp([string]$nombreSitio, [int]$puerto = 21, [string]$ruta) {
-    New-WebFtpSite -Name $nombreSitio -Port $puerto -PhysicalPath $ruta -Force
-}
+# Creación del usuario
+$FTPUserName = "Quintana"
+$FTPPassword = "#contrasena11"
+$CreateUserFTPUser = $ADSI.Create("User", "$FTPUserName")
+$CreateUserFTPUser.SetInfo()
+$CreateUserFTPUser.SetPassword("$FTPPassword")
+$CreateUserFTPUser.SetInfo()
 
-crearGrupo -nombreGrupo "Everyone"
-crearGrupo -nombreGrupo "reprobados"
-crearGrupo -nombreGrupo "recursadores"
+# Unión de los usuarios al grupo FTP
+$UserAccount = New-Object System.Security.Principal.NTAccount("$FTPUserName")
+$SID = $UserAccount.Translate([System.Security.Principal.SecurityIdentifier])
+$Group = [ADSI]"WinNT://$env:ComputerName/$FTPUserGroupName,Group"
+$User = [ADSI]"WinNT://$SID"
+$Group.Add($User.Path)
 
-$rutaGeneral = "C:\FTP\General"
-$rutaReprobados = "C:\FTP\Reprobados"
-$rutaRecursadores = "C:\FTP\Recursadores"
+# Habilitar autenticacion básica
+Set-ItemProperty "IIS:\Sites\FTP" -Name ftpServer.Security.authentication.basicAuthentication.enabled -Value $true
 
-if(!(Test-Path $rutaGeneral)){
-    New-Item -ItemType Directory -Path $rutaGeneral
-}
+# Permisos de lectura y escritura
+Add-WebConfiguration "/system.ftpServer/security/authorization" -value @{acessType="Allow";roles="$FTPUserGroupName";permissions=3} -PSPath IIS:\ -location "FTP"
 
-if(!(Test-Path $rutaReprobados)){
-    New-Item -ItemType Directory -Path $rutaReprobados
-}
+Set-ItemProperty "IIS:\Sites\FTP" -Name ftpServer.security.ssl.controlChannelPolicy -Value 0
+Set-ItemProperty "IIS:\Sites\FTP" -Name ftpServer.security.ssl.dataChannelPolicy -Value 0
 
-if(!(Test-Path $rutaRecursadores)){
-    New-Item -ItemType Directory -Path $rutaRecursadores
-}
-
-Crear-SitioFtp -nombreSitio "ServidorFTP" -ruta $rutaGeneral
-
-$rutaSitioFtp = "IIS:\Sites\ServidorFTP"
-Set-ItemProperty -Path $rutaSitioFtp -Name "ftpServer.security.authentication.anonymousAuthentication.enabled" -Value $true
-
-function Establecer-Permisos([string]$ruta, [string]$grupo) {
-    try {
-        $grupoCompleto = "$env:ComputerName\$grupo"
-        $acl = Get-Acl $ruta
-
-        $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule(
-            $grupoCompleto,
-            "Modify",            
-            "ContainerInherit,ObjectInherit",
-            "None",              
-            "Allow"
-        )
-
-        $acl.AddAccessRule($accessRule)
-
-        Set-Acl -Path $ruta -AclObject $acl
-
-        Write-Host "Permisos establecidos correctamente para el grupo '$grupo' en la ruta '$ruta'."
-    }
-    catch {
-        Write-Host "Ocurrió un error al establecer permisos: $_"
-    }
-}
-
-function Deshabilitar-SSL() {
-    Set-WebConfigurationProperty -Filter "/system.ftpServer/security/ssl" -Name "controlChannelPolicy" -Value "None" -PSPath "IIS:\"
-    Set-WebConfigurationProperty -Filter "/system.ftpServer/security/ssl" -Name "dataChannelPolicy" -Value "None" -PSPath "IIS:\"
-}
-
-Establecer-Permisos -ruta $rutaGeneral -grupo "Everyone"
-Establecer-Permisos -ruta $rutaReprobados -grupo "reprobados"
-Establecer-Permisos -ruta $rutaRecursadores -grupo "recursadores"
-
-function Crear-UsuarioFtp([string]$usuario, [string]$contrasena, [string]$grupo) {
-    $passwordSecure = ConvertTo-SecureString -String $contrasena -AsPlainText -Force
-    New-LocalUser -Name $usuario -Password $passwordSecure -FullName $usuario -Description "Usuario FTP"
-    Add-LocalGroupMember -Group $grupo -Member $usuario
-}
-
-Deshabilitar-SSL -rutaSitioFtp $rutaSitioFtp
-
-Crear-UsuarioFtp -usuario "mrgarza" -contrasena "#password222#" -grupo "reprobados"
-Crear-UsuarioFtp -usuario "mrorozco" -contrasena "#password222#" -grupo "recursadores"
-
-Restart-WebItem "IIS:\Sites\ServidorFTP"
-
-<#while($true){
-    echo "Menu"
-    echo "1. Agregar usuario"
-    echo "2. Cambiar usuario de grupo"
-    echo "3. Salir"
-
-    try{
-        $opcion = Read-Host "Selecciona una opcion"
-        $intOpcion = [int]$opcion
-    }
-    catch{
-        echo "Has ingresado un valor no entero"
-    }
-
-    if($intOpcion -eq 3){
-        echo "Saliendo..."
-        break
-    }
-
-    if($intOpcion -is [int]){
-        switch($opcion){
-            1 {
-                $usuario = Read-Host "Ingresa el nombre de usuario"
-                $password = Read-Host "Ingresa la contrasena" -AsSecureString
-                $grupo = Read-Host "Ingresa el grupo al que pertenecera el usuario (reprobados/recursadores)"
-            }
-            2 {
-                $usuarioACambiar = Read-Host "Ingresa el usuario a cambiar de grupo"
-                $grupo = Read-Host "Ingresa el nuevo grupo del usuario"
-            }
-            default {"Ingresa un numero dentro del rango (1..3)"}
-        }
-    }
-    echo `n
-}#>
+Restart-WebItem "IIS:\Sites\FTP"
