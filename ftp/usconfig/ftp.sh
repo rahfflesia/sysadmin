7ced5e25619d535bc3325985f7e4087304b275db
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-03-05 22:01:45 -0700

    depuracion

--- ftp/usconfig/ftp.sh
+++ ftp/usconfig/ftp.sh
@@ -53,21 +53,18 @@ do
                 sudo usermod -G "$grupo" "$usuario"
 
                 sudo mkdir -p "/home/jj/ftp/usuarios/$usuario/$usuario"
-
                 sudo mkdir -p "/home/jj/ftp/users/$usuario"
-                sudo chmod 775 "/home/jj/ftp/usuarios/$usuario/$usuario"
-
                 sudo mkdir -p "/home/jj/ftp/usuarios/$usuario/general"
-                sudo chmod 775 "/home/jj/ftp/usuarios/$usuario/general"
-
                 sudo mkdir -p "/home/jj/ftp/usuarios/$usuario/$grupo"
-                sudo chmod 775 "/home/jj/ftp/usuarios/$usuario/$grupo"
-
-                sudo chmod -R 775 "/home/jj/ftp/usuarios/$usuario"
 
                 sudo chown "$usuario" "/home/jj/ftp/usuarios/$usuario/$grupo"
                 sudo chown "$usuario" "/home/jj/ftp/usuarios/$usuario/general"
                 sudo chown "$usuario" "/home/jj/ftp/usuarios/$usuario/$usuario"
+
+                sudo chmod 775 "/home/jj/ftp/usuarios/$usuario/$usuario"
+                sudo chmod 775 "/home/jj/ftp/usuarios/$usuario/general"
+                sudo chmod 775 "/home/jj/ftp/usuarios/$usuario/$grupo"
+                sudo chmod -R 775 "/home/jj/ftp/usuarios/$usuario"
 
                 # Enlaces
                 sudo mount --bind "/home/jj/ftp/general" "/home/jj/ftp/usuarios/$usuario/general"
