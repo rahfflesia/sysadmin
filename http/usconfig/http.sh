868e5f0e22b78dc907e1f69cd7d1185a7703a98b
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-03-10 11:41:23 -0700

    script ubuntu server funcional

--- http/usconfig/http.sh
+++ http/usconfig/http.sh
@@ -1,6 +1,7 @@
 # Ya funciona la instalacion de apache, en cualquier inconveniente puedo volver a este commit
 # $1 = URL, Retorna el html
 # Nginx funcional
+# Script de ubuntu 100% funcional, puedo volver a este commit en caso de error
 function hacerPeticion(){
     local url=$1
     local html=$(curl -s "$url")
