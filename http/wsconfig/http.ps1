b51cf93a46d759fff401aabab403425fb6ab0386
rafflesia <154933782+rahfflesia@users.noreply.github.com>
2025-03-20 22:42:31 -0700

    nginx implementar ftp ws

--- http/wsconfig/http.ps1
+++ http/wsconfig/http.ps1
@@ -100,7 +100,7 @@ function listarDirectoriosFtp {
             $respuestaStream = $respuesta.GetResponseStream()
             $lector = New-Object System.IO.StreamReader($respuestaStream)
 
-            Write-Host "Conexión exitosa usando SSL = $usarSsl"
+            Write-Host "Conexion exitosa usando SSL = $usarSsl"
 
             while (-not $lector.EndOfStream) {
                 $linea = $lector.ReadLine()
@@ -311,13 +311,290 @@ https://192.168.100.38:$puerto {
                     "3"{
                         echo "Saliendo del menu de Caddy..."
                     }
+                    default { echo "Selecciona una opcion valida" } 
                 }
             }
             "2"{
-                
+                $nginxDescargas = "https://nginx.org/en/download.html"
+                $paginaNginx = (hacerPeticion -url $nginxDescargas).Content
+                $versiones = (encontrarValor -regex $versionRegex -pagina $paginaNginx)
+                $versionLTSNginx = $versiones[6]
+                $versionDevNginx = $versiones[0]
+
+                echo "Instalador de Nginx"
+                echo "1. Version LTS $versionLTSNginx"
+                echo "2. Version de desarrollo $versionDevNginx"
+                echo "3. Salir"
+                $opcNginx = Read-Host "Selecciona una version"
+                switch($opcNginx){
+                    "1"{
+                            try {
+                            $puerto = Read-Host "Ingresa el puerto donde se realizara la instalacion"
+                            if(-not(Es-Numerico -string $puerto)){
+                                echo "Ingresa un valor numerico entero"
+                            }
+                            elseif(-not(Es-RangoValido $puerto)){
+                                echo "Ingresa un puerto dentro del rango (0-65535)"
+                            }
+                            elseif(Es-PuertoEnUso $puerto){
+                                echo "El puerto se encuentra en uso"
+                            }
+                            elseif(-not(Es-PuertoValido $puerto)){
+                                echo "Error"
+                            }
+                            else{
+                                $opcSsl = Read-Host "Quieres habilitar SSL (si/no)"
+                                if($opcSsl.ToLower() -eq "si"){
+                                    echo "Habilitando SSL..."
+                                    Stop-Process -Name nginx -ErrorAction SilentlyContinue
+                                    echo "Instalando version LTS $versionLTSNginx"
+                                    Invoke-WebRequest -UseBasicParsing "$servidorFtp/Nginx/nginx-$versionLTSNginx.zip" -Outfile "C:\descargas\nginx-$versionLTSNginx.zip"
+                                    Expand-Archive C:\descargas\nginx-$versionLTSNginx.zip C:\descargas -Force
+                                    cd C:\descargas\nginx-$versionLTSNginx
+                                    Clear-Content -Path "C:\descargas\nginx-$versionLTSNginx\conf\nginx.conf"
+                                    Start-Process nginx.exe
+                                    Get-Process | Where-Object { $_.ProcessName -like "*nginx*" }
+                                    cd ..
+                                    $contenido = @"
+worker_processes  1;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    sendfile        on;
+    keepalive_timeout  65;
+
+    # Configuración del servidor HTTP (redirige a HTTPS)
+    server {
+        listen 81;
+        server_name localhost;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+    }
+
+    # Configuración del servidor HTTPS
+    server {
+        listen $puerto ssl;
+        server_name localhost;
+
+        ssl_certificate c:\descargas\certificate.crt;
+        ssl_certificate_key c:\descargas\private.key;
+
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ciphers HIGH:!aNULL:!MD5;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+    }
+}
+"@
+                                    Set-Content -Path "C:\descargas\nginx-$versionLTSNginx\conf\nginx.conf" -Value $contenido
+                                    netsh advfirewall firewall add rule name="Nginx" dir=in action=allow protocol=TCP localport=$puerto
+                                    echo "Se instalo la version LTS $versionLTSNginx de Nginx"
+                                }
+                                elseif($opcSsl.ToLower() -eq "no"){
+                                    echo "SSL no se habilitara"
+                                    Stop-Process -Name nginx -ErrorAction SilentlyContinue
+                                    echo "Instalando version LTS $versionLTSNginx"
+                                    Invoke-WebRequest -UseBasicParsing "$servidorFtp/Nginx/nginx-$versionLTSNginx.zip" -Outfile "C:\descargas\nginx-$versionLTSNginx.zip"
+                                    Expand-Archive C:\descargas\nginx-$versionLTSNginx.zip C:\descargas -Force
+                                    cd C:\descargas\nginx-$versionLTSNginx
+                                    Clear-Content -Path "C:\descargas\nginx-$versionLTSNginx\conf\nginx.conf"
+                                    Start-Process nginx.exe
+                                    Get-Process | Where-Object { $_.ProcessName -like "*nginx*" }
+                                    cd ..
+                                    $contenido = @"
+worker_processes  1;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    sendfile        on;
+    keepalive_timeout  65;
+
+    # Configuración del servidor HTTP (redirige a HTTPS)
+    server {
+        listen $puerto;
+        server_name localhost;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+    }
+}
+"@
+                                    Set-Content -Path "C:\descargas\nginx-$versionLTSNginx\conf\nginx.conf" -Value $contenido
+                                    netsh advfirewall firewall add rule name="Nginx" dir=in action=allow protocol=TCP localport=$puerto
+                                    echo "Se instalo la version LTS $versionLTSNginx de Nginx"
+                                }
+                                else{
+                                    echo "Selecciona una opcion valida (si/no)"
+                                }
+                            }
+                        }
+                        catch {
+                            Echo $Error[0].ToString()
+                        }
+                    }
+                    "2"{
+                        try {
+                            $puerto = Read-Host "Ingresa el puerto donde se realizara la instalacion"
+                            if(-not(Es-Numerico -string $puerto)){
+                                echo "Ingresa un valor numerico entero"
+                            }
+                            elseif(-not(Es-RangoValido $puerto)){
+                                echo "Ingresa un puerto dentro del rango (0-65535)"
+                            }
+                            elseif(Es-PuertoEnUso $puerto){
+                                echo "El puerto se encuentra en uso"
+                            }
+                            elseif(-not(Es-PuertoValido $puerto)){
+                                echo "Error"
+                            }
+                            else{
+                                $opcSsl = Read-Host "Quieres activar SSL? (si/no)"
+                                if($opcSsl.ToLower() -eq "si"){
+                                    echo "Habilitando SSL..."
+                                    Stop-Process -Name nginx -ErrorAction SilentlyContinue
+                                    echo "Instalando version LTS $versionDevNginx"
+                                    Invoke-WebRequest -UseBasicParsing "$servidorFtp/Nginx/nginx-$versionDevNginx.zip" -Outfile "C:\descargas\nginx-$versionDevNginx.zip"
+                                    Expand-Archive C:\descargas\nginx-$versionDevNginx.zip C:\descargas -Force
+                                    cd C:\descargas\nginx-$versionDevNginx
+                                    Clear-Content -Path "C:\descargas\nginx-$versionDevNginx\conf\nginx.conf"
+                                    Start-Process nginx.exe
+                                    Get-Process | Where-Object { $_.ProcessName -like "*nginx*" }
+                                    cd ..
+                                    $contenido = @"
+worker_processes  1;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    sendfile        on;
+    keepalive_timeout  65;
+
+    # Configuración del servidor HTTP (redirige a HTTPS)
+    server {
+        listen 81;
+        server_name localhost;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+    }
+
+    # Configuración del servidor HTTPS
+    server {
+        listen $puerto ssl;
+        server_name localhost;
+
+        ssl_certificate c:\descargas\certificate.crt;
+        ssl_certificate_key c:\descargas\private.key;
+
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ciphers HIGH:!aNULL:!MD5;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+    }
+}
+"@
+                                    Set-Content -Path "C:\descargas\nginx-$versionDevNginx\conf\nginx.conf" -Value $contenido
+                                    netsh advfirewall firewall add rule name="Nginx" dir=in action=allow protocol=TCP localport=$puerto
+                                    echo "Se instalo la version LTS $versionDevNginx de Nginx"
+
+                                }
+                                elseif($opcSsl.ToLower() -eq "no"){
+                                    echo "SSL no se habilitara"
+                                    Stop-Process -Name nginx -ErrorAction SilentlyContinue
+                                    echo "Instalando version LTS $versionDevNginx"
+                                    Invoke-WebRequest -UseBasicParsing "$servidorFtp/Nginx/nginx-$versionDevNginx.zip" -Outfile "C:\descargas\nginx-$versionDevNginx.zip"
+                                    Expand-Archive C:\descargas\nginx-$versionDevNginx.zip C:\descargas -Force
+                                    cd C:\descargas\nginx-$versionDevNginx
+                                    Clear-Content -Path "C:\descargas\nginx-$versionDevNginx\conf\nginx.conf"
+                                    Start-Process nginx.exe
+                                    Get-Process | Where-Object { $_.ProcessName -like "*nginx*" }
+                                    cd ..
+                                    $contenido = @"
+worker_processes  1;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    sendfile        on;
+    keepalive_timeout  65;
+
+    # Configuración del servidor HTTP (redirige a HTTPS)
+    server {
+        listen $puerto;
+        server_name localhost;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+    }
+}
+"@
+                                    Set-Content -Path "C:\descargas\nginx-$versionDevNginx\conf\nginx.conf" -Value $contenido
+                                    netsh advfirewall firewall add rule name="Nginx" dir=in action=allow protocol=TCP localport=$puerto
+                                    echo "Se instalo la version LTS $versionDevNginx de Nginx"
+                                }
+                                else{
+                                    echo "Selecciona una opcion valida (si/no)"
+                                }
+                            }
+                        }
+                        catch {
+                            echo $Error[0].ToString()
+                        }
+                    }
+                    "3"{
+                        echo "Saliendo del menu de Nginx..."
+                    }
+                }
             }
             "3"{
-            
+                echo "Otros"
             }
             default { 
                 echo "Selecciona una opcion dentro del rango (1..4)" 
